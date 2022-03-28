6e7287f32532ff681be229dc5e6b0d515b10945a
CCkyen <chanel.suh1@gmail.com>
2022-03-28 11:40:59 +0200

    Main Branch

---
+++ footer.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    ...
+
+    <!-- Font Awesome JS -->
+    <script src="https://kit.fontawesome.com/4df41d2524.js" crossorigin="anonymous"></script>
+    <title>Bootstrap Footer Examples 2020</title>
+  </head>
+  <body>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    ...
+  </body>
+
+  <footer></footer>
+</html>

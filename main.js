1cb900eb64be52e8262fe24022b8d67012c05a22
julianpetrini <bwmisojos@gmail.com>
2022-03-29 15:04:52 +0200

    JS VALIDATE Q1 (DONE)

---
+++ main.js
@@ -0,0 +1,83 @@
+/*
+THIS WAS THE PATH TO THE CORRECT FUNCTION OF VALIDATION AND TO CHOOSE AN ANSWER
+
+function questionTry(){
+    if(document.getElementById('question1')===undefined){
+        alert("you are a gil");
+    }
+
+    if(document.getElementById('question1').checked =! 0){
+        alert("wrong answer"); 
+    }
+
+    else if (document.getElementById('question').checked == false) {   
+        alert("Summer radio button is selected");   
+    } 
+    else {  
+       return false;
+            }  
+    
+}
+
+function prueba(){
+    let myElement = document.getElementById("question1").value;
+
+    
+        alert(myElement);
+}
+
+function prueba2(){
+    if(document.getElementById('correct').checked == true) {   
+        alert("GREAT ANSWER");   
+    }
+    else if (document.getElementById('option1').checked == true){
+        alert("WRONG ANSWER"); 
+        return false;
+    }
+    else if (document.getElementById('option2').checked == true){
+        alert("WRONG ANSWER");
+        return false;
+    }
+    else if (document.getElementById('option3').checked == true){
+        alert("WRONG ANSWER"); 
+        return false;
+    }
+
+    else if (document.getElementById()=!checked){
+        alert("GIVE AN ASNWER IN ORDER TO CONTINUE"); 
+        return false;
+    }
+    
+}
+*/
+
+function fragen1(){
+    var getSelectedValue = document.querySelector( 'input[name="question1"]:checked'); 
+
+    
+    if(getSelectedValue === null) {   
+        alert("Choose an option please");  
+        return false;
+    }
+    else if(getSelectedValue===document.getElementById('correct')){
+        alert("YOU ARE AMAZING");  
+    }
+    else if(getSelectedValue===document.getElementById('incorrect')){
+        alert("YOU SHOULD STUDY");
+        window.open('https://www.worldometers.info/geography/how-many-countries-are-there-in-the-world/#:~:text=There%20are%20195%20countries%20in,and%20the%20State%20of%20Palestine.','_blank');
+        return false;
+    }
+    else if(getSelectedValue===document.getElementById('incorrect1')){
+        alert("YOU SHOULD STUDY");
+        window.open('https://www.worldometers.info/geography/how-many-countries-are-there-in-the-world/#:~:text=There%20are%20195%20countries%20in,and%20the%20State%20of%20Palestine.','_blank');        return false;
+    }
+    else if(getSelectedValue===document.getElementById('incorrect2')){
+        alert("YOU SHOULD STUDY");
+        window.open('https://www.worldometers.info/geography/how-many-countries-are-there-in-the-world/#:~:text=There%20are%20195%20countries%20in,and%20the%20State%20of%20Palestine.','_blank');        return false;
+    }
+}
+
+function setWarning(text) {
+    let warningElement = document.getElementById("validation-warning");
+    warningElement.innerText = text;
+}

9da2465cd358aa664830107bec15d49b9a42b7f7
AliFilliol <gisele.dasilva.filliol@gmail.com>
2022-03-30 16:12:32 +0200

    30 March Tool

--- main.js
+++ main.js
@@ -49,6 +49,64 @@ function fragen2(){
     }
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // JS - Ali Filliol
 function fragen3(){
     let getSelectedValue = document.querySelector( 'input[name="question3"]:checked'); 
@@ -77,27 +135,29 @@ function fragen3(){
 }
 
 function fragen4(){
-let getSelectedValue = document.querySelector( 'input [name="question4"]:checked');
+    let getSelectedValue = document.querySelector( 'input[name="question4"]:checked'); 
 
-if(getSelectedValue === null) {
-    alert("Choose an option please");
-    return false;
-}
-else if(getSelectedValue===document.getElementById('correct')){
-    alert("YOU ARE BRAINY!!!");
-    
-}
-else if(getSelectedValue===document.getElementById('incorrect')){
-    alert("YOU SHOULD STUDY MORE, HERE YOU CAN LEARN ABOUT IT");
-    window.open('https://www.britannica.com/place/Canberra/','_blank');
+    if(getSelectedValue === null) {
+        alert("Choose an option please");
+        return false;
+    }
+    else if(getSelectedValue===document.getElementById('correct')){
+        alert("YOU ARE BRAINY!!!");
+        
+    }
+    else if(getSelectedValue===document.getElementById('incorrect')){
+        alert("YOU SHOULD STUDY MORE, HERE YOU CAN LEARN ABOUT IT");
+        window.open('https://www.britannica.com/place/Canberra','_blank');
 
+    }
+    else if(getSelectedValue===document.getElementById('incorrect1')){
+        alert("PLEASE CHECK THE PAGE BELOW");
+        window.open('https://www.britannica.com/place/Canberra','_blank'); 
+    }
+    else if(getSelectedValue===document.getElementById('incorrect2')){
+        alert("PLEASE CHECK THE PAGE BELOW");
+        window.open('https://www.britannica.com/place/Canberra','_blank');
+    }
 }
-else if(getSelectedValue===document.getElementById('incorrect1')){
-    alert("PLEASE CHECK THE PAGE BELOW");
-    window.open('https://www.britannica.com/place/Canberra/','_blank'); 
-}
-else if(getSelectedValue===document.getElementById('incorrect2')){
-    alert("PLEASE CHECK THE PAGE BELOW");
-    window.open('https://www.britannica.com/place/Canberra/','_blank');
-}
-}
+
+
